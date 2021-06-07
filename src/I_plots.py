0392f2d62fc453ed2cfc9dc8d619a77d5772d753
sasa821 <s.sayadi@protonmail.com>
2021-06-07 14:42:29 +0200

    slight changes in the plot function

--- src/I_plots.py
+++ src/I_plots.py
@@ -8,12 +8,12 @@ import pandas as pd
 df = pd.read_csv("../simulation_results/results.csv",
     delimiter=",",
 )
-
 # Plot
 df_cs1 = df[df["case"]=="pv-diesel-storage-mg"]
-df_cs2 = df[df["case"]=="pv-storage-mg"]
+df_cs2 = df[df["case"]=="diesel_mg"]
+df_cs3 = df[df["case"]=="pv-storage-mg"]
 x = df_cs1["fuel_price"]
-plt.plot(x, df_cs1["lcoe"], x, df_cs2["lcoe"])
+plt.plot(x, df_cs1["lcoe"], x, df_cs2["lcoe"], x, df_cs3["lcoe"])
 plt.xlabel("Ful Price")
 plt.ylabel("LCOE [$/kWh]")
 plt.show()
